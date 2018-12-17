db99a913b03748cd0e2de5744b424bbad727534a
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-12-17 12:53:28 +0100

    model user added.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-require('dotenv').config()
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 // const fs = require('fs-extra');  // file system
 const PORT = process.env.PORT
 const HOST = process.env.HOST
 
+app.use(cors())
+app.use(bodyParser.json())
+
 app.post('/login', (req, res) => {
-	//...
+	//authenticate user
+	//si ok, générer token
 })
 
 app.post('/register', (req, res) => {
@@ -17,7 +23,8 @@ app.post('/register', (req, res) => {
 
 // app.listen(PORT, HOST, () => console.log("app démarrée sur localhost:9000"))
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env
+const connectionString = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
 
 const startApp = app => {
 	return new Promise((resolve, reject) => {
@@ -25,3 +32,7 @@ const startApp = app => {
 		server.on('error', reject)
 	})
 }
+
+mongoose.connect( connectionString )
+	.then(() => startApp(app))
+	.catch(err => console.log(err));
